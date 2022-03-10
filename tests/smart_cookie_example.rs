fd9e967124ab9a49b3a102055627e1887f2d6ae2
Frederic Kettelhoit <kettelhoit@gmail.com>
2022-03-10 15:00:23 +0100

    test: add 2nd test case for smart cookie example

--- tests/smart_cookie_example.rs
+++ tests/smart_cookie_example.rs
@@ -9,7 +9,7 @@ use garble::{
 };
 
 #[test]
-fn smart_cookie_interaction() -> Result<(), Error> {
+fn smart_cookie_simple_interaction() -> Result<(), Error> {
     let init = read_source_code("init.garble.rs");
     let log_interest = read_source_code("log_interest.garble.rs");
     let decide_ad = read_source_code("decide_ad.garble.rs");
@@ -58,24 +58,12 @@ fn smart_cookie_interaction() -> Result<(), Error> {
             .map_err(|e| pretty_print(e, &log_interest))?
             .into_literal(&log_interest_checked)
             .map_err(|e| pretty_print(e, &log_interest))?;
-        user_state = match &log_interest_result {
-            Literal::Enum(enum_name, variant_name, fields) => {
-                match (enum_name.as_str(), variant_name.as_str()) {
-                    ("LogInterestResult", "Ok") => match fields {
-                        VariantLiteral::Tuple(fields) if fields.len() == 2 => {
-                            Literal::Tuple(fields.clone())
-                        }
-                        _ => {
-                            panic!("Expected a variant with 2 fields, found {fields:?}")
-                        }
-                    },
-                    _ => {
-                        panic!("Unexpected enum: {log_interest_result}")
-                    }
-                }
-            }
-            result => panic!("Expected an enum result, but found {result}"),
-        };
+        user_state = Literal::Tuple(expect_enum(
+            &log_interest_result,
+            "LogInterestResult",
+            "Ok",
+            Some(2),
+        ));
     }
 
     let mut decide_ad_eval = Evaluator::from(&decide_ad_circuit);
@@ -91,33 +79,129 @@ fn smart_cookie_interaction() -> Result<(), Error> {
         .into_literal(&decide_ad_checked)
         .map_err(|e| pretty_print(e, &decide_ad))?;
 
-    match &ad_decision {
-        Literal::Enum(enum_name, variant_name, fields) => {
-            match (enum_name.as_str(), variant_name.as_str()) {
-                ("AdDecisionResult", "Ok") => match fields {
-                    VariantLiteral::Tuple(fields) if fields.len() == 1 => {
-                        assert_eq!(
-                            fields[0],
-                            Literal::Enum(
-                                "UserInterest".to_string(),
-                                "Cars".to_string(),
-                                VariantLiteral::Unit
-                            )
-                        )
-                    }
-                    _ => {
-                        panic!("Expected a variant with 1 field, found {fields:?}")
-                    }
-                },
-                _ => {
-                    panic!("Unexpected enum: {ad_decision}");
-                }
-            }
-        }
-        _ => panic!("Unexpected ad decision result: {ad_decision}"),
+    let ad_decision = expect_enum(&ad_decision, "AdDecisionResult", "Ok", Some(1));
+    assert_eq!(
+        ad_decision[0],
+        Literal::Enum(
+            "UserInterest".to_string(),
+            "Cars".to_string(),
+            VariantLiteral::Unit
+        )
+    );
+    Ok(())
+}
+
+#[test]
+fn smart_cookie_larger_than_buffer_interaction() -> Result<(), Error> {
+    let init = read_source_code("init.garble.rs");
+    let log_interest = read_source_code("log_interest.garble.rs");
+    let decide_ad = read_source_code("decide_ad.garble.rs");
+
+    let init_checked = check(&init).map_err(|e| pretty_print(e, &init))?;
+    let log_interest_checked = check(&log_interest).map_err(|e| pretty_print(e, &log_interest))?;
+    let decide_ad_checked = check(&decide_ad).map_err(|e| pretty_print(e, &decide_ad))?;
+
+    let init_circuit = compile(&init)?;
+    let log_interest_circuit = compile(&log_interest)?;
+    let decide_ad_circuit = compile(&decide_ad)?;
+
+    let website_signing_key = Literal::NumUnsigned(0, UnsignedNumType::U128);
+
+    let mut init_eval = Evaluator::from(&init_circuit);
+    init_eval
+        .parse_literal(&init_checked, "()")
+        .map_err(|e| pretty_print(e, &init))?;
+    init_eval
+        .set_literal(&init_checked, website_signing_key.clone())
+        .map_err(|e| pretty_print(e, &init))?;
+    let mut user_state = init_eval
+        .run()
+        .map_err(|e| pretty_print(e, &init))?
+        .into_literal(&init_checked)
+        .map_err(|e| pretty_print(e, &init))?;
+
+    for interest in ["Politics"; 20].iter().chain(["Luxury"; 500].iter()) {
+        let mut log_interest_eval = Evaluator::from(&log_interest_circuit);
+        log_interest_eval
+            .set_literal(&log_interest_checked, user_state)
+            .map_err(|e| pretty_print(e, &log_interest))?;
+        let interest = Literal::Enum(
+            "UserInterest".to_string(),
+            interest.to_string(),
+            VariantLiteral::Unit,
+        );
+        log_interest_eval
+            .set_literal(
+                &log_interest_checked,
+                Literal::Tuple(vec![interest, website_signing_key.clone()]),
+            )
+            .map_err(|e| pretty_print(e, &log_interest))?;
+        let log_interest_result = log_interest_eval
+            .run()
+            .map_err(|e| pretty_print(e, &log_interest))?
+            .into_literal(&log_interest_checked)
+            .map_err(|e| pretty_print(e, &log_interest))?;
+        user_state = Literal::Tuple(expect_enum(
+            &log_interest_result,
+            "LogInterestResult",
+            "Ok",
+            Some(2),
+        ));
     }
 
+    let mut decide_ad_eval = Evaluator::from(&decide_ad_circuit);
+    decide_ad_eval
+        .set_literal(&decide_ad_checked, user_state)
+        .map_err(|e| pretty_print(e, &decide_ad))?;
+    decide_ad_eval
+        .set_literal(&decide_ad_checked, website_signing_key)
+        .map_err(|e| pretty_print(e, &decide_ad))?;
+    let ad_decision = decide_ad_eval
+        .run()
+        .map_err(|e| pretty_print(e, &decide_ad))?
+        .into_literal(&decide_ad_checked)
+        .map_err(|e| pretty_print(e, &decide_ad))?;
+
+    let ad_decision = expect_enum(&ad_decision, "AdDecisionResult", "Ok", Some(1));
+    assert_eq!(
+        ad_decision[0],
+        Literal::Enum(
+            "UserInterest".to_string(),
+            "Luxury".to_string(),
+            VariantLiteral::Unit
+        )
+    );
     Ok(())
+}
+
+fn expect_enum(
+    l: &Literal,
+    enum_name: &str,
+    variant_name: &str,
+    fields: Option<usize>,
+) -> Vec<Literal> {
+    match &l {
+        Literal::Enum(actual_enum_name, actual_variant_name, actual_fields) => {
+            if actual_enum_name.as_str() == enum_name
+                && actual_variant_name.as_str() == variant_name
+            {
+                match actual_fields {
+                    VariantLiteral::Tuple(actual_fields)
+                        if fields.is_some() && fields.unwrap() == actual_fields.len() =>
+                    {
+                        actual_fields.clone()
+                    }
+                    VariantLiteral::Unit if fields.is_none() => vec![],
+                    _ => {
+                        panic!("Expected a variant with {fields:?} fields, found {actual_fields:?}")
+                    }
+                }
+            } else {
+                panic!("Unexpected enum: {l}");
+            }
+        }
+        _ => panic!("Unexpected ad decision result: {l}"),
+    }
 }
 
 fn read_source_code(file_name: &str) -> String {
