6a923196d78ea555fa5c11fe6041eacdf6d9669c
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-09-12 15:11:56 +0100

    fix: function names, indentation and documentation

--- tests/credit_scoring_example.rs
+++ tests/credit_scoring_example.rs
@@ -1,18 +1,11 @@
 use std::{fs::File, io::Read};
 
-use garble_lang::{
-    ast::Type,
-    check,
-    circuit::Circuit,
-    // compile,
-    eval::Evaluator,
-    literal::{Literal},
-    // token::{SignedNumType, UnsignedNumType},
-    Error,
-};
+use garble_lang::{ast::Type, check, circuit::Circuit, eval::Evaluator, literal::Literal, Error};
 
 #[test]
-fn credit_scoring_compilation() -> Result<(), Error> {
+// Tests whether successive compilations always result in the same circuit, rather than different circuits with the same
+// output
+fn credit_scoring_successive_compilations() -> Result<(), Error> {
     let credit_scoring = read_source_code("credit_scoring.garble.rs");
     println!("Parsing and type-checking...");
     let typed_prg = check(&credit_scoring).map_err(|e| pretty_print(e, &credit_scoring))?;
@@ -51,7 +44,7 @@ fn credit_scoring_compilation() -> Result<(), Error> {
 }
 
 #[test]
-fn credit_scoring_global() -> Result<(), Error> {
+fn credit_scoring_interaction() -> Result<(), Error> {
     let credit_scoring = read_source_code("credit_scoring.garble.rs");
     println!("Parsing and type-checking...");
     let typed_prg = check(&credit_scoring).map_err(|e| pretty_print(e, &credit_scoring))?;
@@ -62,76 +55,77 @@ fn credit_scoring_global() -> Result<(), Error> {
         &typed_prg,
         &user_ty,
         "User {
-                      age: 37u8,
-                      income: 5500u32,
-                      account_balance: 25000i64,
-                      current_loans: 60000u64,
-                      credit_card_limit: 1000u32,
-                      ever_bankrupt: false,
-                      loan_payment_failures: 0u8,
-                      credit_payment_failures: 2u8,
-                      surety_income: 5000u32
-                    }",
+          age: 37u8,
+          income: 5500u32,
+          account_balance: 25000i64,
+          current_loans: 60000u64,
+          credit_card_limit: 1000u32,
+          ever_bankrupt: false,
+          loan_payment_failures: 0u8,
+          credit_payment_failures: 2u8,
+          surety_income: 5000u32
+        }",
     )?;
 
     let scoring_algorithm_ty = Type::Struct("ScoringAlgorithm".to_string());
     let scoring_algorithm = Literal::parse(
-      &typed_prg,
-      &scoring_algorithm_ty,
-      "ScoringAlgorithm {
-                      age_score: [
-                        MatchClause::Range(Range {min: 0i64, max: 18i64}, Points {inc: 0i32}),
-                        MatchClause::Range(Range {min: 18i64, max: 35i64}, Points {inc: 50i32}),
-                        MatchClause::Range(Range {min: 35i64, max: 65i64}, Points {inc: 100i32}),
-                        MatchClause::Range(Range {min: 65i64, max: 120i64}, Points {inc: 50i32})
-                        ],
-                      income_score: [
-                        MatchClause::Range(Range {min: 2000i64, max: 5000i64}, Points {inc: 50i32}),
-                        MatchClause::Range(Range {min: 5000i64, max: 10000i64}, Points {inc: 100i32}),
-                        MatchClause::Range(Range {min: 10000i64, max: 999999999i64}, Points {inc: 200i32}),
-                        MatchClause::None
-                        ],
-                      account_balance_score: [
-                        MatchClause::Range(Range {min: -999999999i64, max: 0i64}, Points {inc: -100i32}),
-                        MatchClause::Range(Range {min: 0i64, max:5000i64}, Points {inc: 0i32}),
-                        MatchClause::Range(Range {min: 5000i64, max: 10000i64}, Points {inc: 50i32}),
-                        MatchClause::Range(Range {min: 10000i64, max: 999999999i64}, Points {inc: 200i32})
-                        ],
-                      current_loans_score: [
-                        MatchClause::Range(Range {min:500000i64, max: 100000000i64}, Points {inc: 0i32}),
-                        MatchClause::Range(Range {min:100000i64, max: 500000i64}, Points {inc: 150i32}),
-                        MatchClause::Range(Range {min:0i64, max: 100000i64}, Points {inc: 300i32}),
-                        MatchClause::None],
-                      credit_card_score: [
-                        MatchClause::Range(Range {min: 0i64, max: 10000i64}, Points {inc: 100i32}),
-                        MatchClause::None,
-                        MatchClause::None,
-                        MatchClause::None
-                        ],
-                      bankruptcy_score: [
-                        MatchClause::Bool(true, Points {inc: -100i32}),
-                        MatchClause::Bool(false, Points {inc: 50i32})
-                        ],
-                      loan_payment_history_score: [
-                        MatchClause::Range(Range {min: 0i64, max: 3i64}, Points {inc: 0i32}),
-                        MatchClause::Range(Range {min: 3i64, max: 6i64}, Points {inc: -100i32}),
-                        MatchClause::None,
-                        MatchClause::None
-                        ],
-                      credit_payment_history_score: [
-                        MatchClause::Range(Range {min: 0i64, max: 1i64}, Points {inc: 0i32}),
-                        MatchClause::Range(Range {min: 1i64, max: 3i64}, Points {inc: -100i32}),
-                        MatchClause::Range(Range {min: 3i64, max: 6i64}, Points {inc: -200i32}),
-                        MatchClause::None
-                        ],
-                      surety_income_score: [
-                        MatchClause::Range(Range {min: 0i64, max: 1000i64}, Points {inc: -50i32}),
-                        MatchClause::Range(Range {min: 1000i64, max: 5000i64}, Points {inc: 0i32}),
-                        MatchClause::Range(Range {min: 5000i64, max: 10000i64}, Points {inc: 100i32}),
-                        MatchClause::None
-                        ],
-                      score_limits: ScoreLimits {min: 0i32, max: 1000i32}}"
-                            )?;
+        &typed_prg,
+        &scoring_algorithm_ty,
+        "ScoringAlgorithm {
+          age_score: [
+            MatchClause::Range(Range {min: 0i64, max: 18i64}, Points {inc: 0i32}),
+            MatchClause::Range(Range {min: 18i64, max: 35i64}, Points {inc: 50i32}),
+            MatchClause::Range(Range {min: 35i64, max: 65i64}, Points {inc: 100i32}),
+            MatchClause::Range(Range {min: 65i64, max: 120i64}, Points {inc: 50i32})
+            ],
+          income_score: [
+            MatchClause::Range(Range {min: 2000i64, max: 5000i64}, Points {inc: 50i32}),
+            MatchClause::Range(Range {min: 5000i64, max: 10000i64}, Points {inc: 100i32}),
+            MatchClause::Range(Range {min: 10000i64, max: 999999999i64}, Points {inc: 200i32}),
+            MatchClause::None
+            ],
+          account_balance_score: [
+            MatchClause::Range(Range {min: -999999999i64, max: 0i64}, Points {inc: -100i32}),
+            MatchClause::Range(Range {min: 0i64, max:5000i64}, Points {inc: 0i32}),
+            MatchClause::Range(Range {min: 5000i64, max: 10000i64}, Points {inc: 50i32}),
+            MatchClause::Range(Range {min: 10000i64, max: 999999999i64}, Points {inc: 200i32})
+            ],
+          current_loans_score: [
+            MatchClause::Range(Range {min:500000i64, max: 100000000i64}, Points {inc: 0i32}),
+            MatchClause::Range(Range {min:100000i64, max: 500000i64}, Points {inc: 150i32}),
+            MatchClause::Range(Range {min:0i64, max: 100000i64}, Points {inc: 300i32}),
+            MatchClause::None],
+          credit_card_score: [
+            MatchClause::Range(Range {min: 0i64, max: 10000i64}, Points {inc: 100i32}),
+            MatchClause::None,
+            MatchClause::None,
+            MatchClause::None
+            ],
+          bankruptcy_score: [
+            MatchClause::Bool(true, Points {inc: -100i32}),
+            MatchClause::Bool(false, Points {inc: 50i32})
+            ],
+          loan_payment_history_score: [
+            MatchClause::Range(Range {min: 0i64, max: 3i64}, Points {inc: 0i32}),
+            MatchClause::Range(Range {min: 3i64, max: 6i64}, Points {inc: -100i32}),
+            MatchClause::None,
+            MatchClause::None
+            ],
+          credit_payment_history_score: [
+            MatchClause::Range(Range {min: 0i64, max: 1i64}, Points {inc: 0i32}),
+            MatchClause::Range(Range {min: 1i64, max: 3i64}, Points {inc: -100i32}),
+            MatchClause::Range(Range {min: 3i64, max: 6i64}, Points {inc: -200i32}),
+            MatchClause::None
+            ],
+          surety_income_score: [
+            MatchClause::Range(Range {min: 0i64, max: 1000i64}, Points {inc: -50i32}),
+            MatchClause::Range(Range {min: 1000i64, max: 5000i64}, Points {inc: 0i32}),
+            MatchClause::Range(Range {min: 5000i64, max: 10000i64}, Points {inc: 100i32}),
+            MatchClause::None
+            ],
+          score_limits: ScoreLimits {min: 0i32, max: 1000i32}
+        }",
+    )?;
 
     let (compute_score_circuit, compute_score_fn) = typed_prg.compile("compute_score")?;
     let mut eval = Evaluator::new(&typed_prg, &compute_score_fn, &compute_score_circuit);
